2e66196d522b6ec3200380690bb656d4ef3f3035
Nathan Chere <nathan@norfen.troten77>
2016-06-22 00:54:17 +0200

    Add prompt explaining what password is asked for during github config

--- configure/github.sh
+++ configure/github.sh
@@ -1,3 +1,4 @@
 drawHeader "Configuring GitHub"
+echo "Please enter the password for your Github account ($GITHUBUSERNAME)"
 curl -u "$GITHUBUSERNAME" --data "{\"title\":\"DevVm_`date +%Y%m%d%H%M%S`\",\"key\":\"`cat ~/.ssh/id_rsa.pub`\"}" https://api.github.com/user/keys
 
