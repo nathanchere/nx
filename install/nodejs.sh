6916ee0c15d8fd6b649fe725fe35aed445c8693b
Nathan Chere <nathan@norfen.troten77>
2016-06-22 00:15:23 +0200

    Fix output redirect bug for nodejsinstaller

--- install/nodejs.sh
+++ install/nodejs.sh
@@ -6,8 +6,9 @@ removerepo "chris-lea/node.js"
 addrawpre "wget -qO- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -"
 addrawpre "wget -qO /dev/null -o /dev/null 'https://deb.nodesource.com/node_6.x/dists/xenial/Release'"
 addrawpre "sudo rm -f /etc/apt/sources.list.d/nodesource.list"
-addrawpre "sudo echo 'deb https://deb.nodesource.com/node_6.x xenial main' > /etc/apt/sources.list.d/nodesource.list"
-addrawpre "sudo echo 'deb-src https://deb.nodesource.com/node_6.x xenial main' >> /etc/apt/sources.list.d/nodesource.list"
+
+addrawpre "sudo bash -c \"echo 'deb https://deb.nodesource.com/node_6.x xenial main' > /etc/apt/sources.list.d/nodesource.list\""
+addrawpre "sudo bash -c \"echo 'deb-src https://deb.nodesource.com/node_6.x xenial main' >> /etc/apt/sources.list.d/nodesource.list\""
 
 addpkg apt-transport-https lsb-release curl nodejs npm nodejs-legacy
 
