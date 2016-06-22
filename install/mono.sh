effa9e07b1ead9a38b9d9e17a4e03c59086a5289
Nathan Chere <git@nathanchere.com.au>
2016-06-22 08:03:34 +0200

    Prevent duplicate mono sources

--- install/mono.sh
+++ install/mono.sh
@@ -1,6 +1,7 @@
 addrawpre "sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF"
-addrawpre "echo \"deb http://download.mono-project.com/repo/debian wheezy main\" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list"
 
+addrawpre "sudo rm -f /etc/apt/sources.list.d/mono-xamarin.list"
+addrawpre "echo \"deb http://download.mono-project.com/repo/debian wheezy main\" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list"
 addrawpre "echo \"deb http://download.mono-project.com/repo/debian wheezy-apache24-compat main\" | sudo tee -a /etc/apt/sources.list.d/mono-xamarin.list"
 
 addpkg mono-devel mono-complete mono-xsp4
